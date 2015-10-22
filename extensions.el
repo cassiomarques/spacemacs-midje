fe90763803447be367285b95ed1b7253ec60537a
Cássio Marques <cassiommc@gmail.com>
2015-10-22 12:47:32 +0100

    Initial commit

---
+++ extensions.el
@@ -0,0 +1,22 @@
+;;; extensions.el --- midje Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq spacemacs-midje-pre-extensions
+      '(
+        ;; pre extension names go here
+        ))
+
+(setq spacemacs-midje-post-extensions
+      '(
+        ;; post extension names go here
+        ))
+
